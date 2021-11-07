b116f80ada0a098f0c675fc6eff01dea7a9795a1
Emitaz-pias <61836021+Emitaz-pias@users.noreply.github.com>
2021-11-07 18:39:06 +0600

    all done

--- src/component/NavMenu.js
+++ src/component/NavMenu.js
@@ -1,23 +1,37 @@
-import React from 'react'
+import React from "react";
 import { Container, Navbar } from "react-bootstrap";
-import logo from "../Image/Logo-Final.png"
+import logo from "../Image/Logo-Final.png";
 
 const NavMenu = () => {
-    return (
-            <Navbar bg="secondary" sticky="top" variant="dark">
-                <Container>
-                    <Navbar.Brand>
-                        <img src={logo} alt="" style={{ maxWidth: "100px", height: "100%" }} />
-                    </Navbar.Brand>
-                    <Navbar.Toggle />
-                    <Navbar.Collapse className="justify-content-end">
-                        <Navbar.Text>
-                            Go Back : <a style={{ textDecoration: "none" }} href="/">Home</a>
-                        </Navbar.Text>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
-    )
-}
+  return (
+    <Navbar bg="white" sticky="top" variant="dark">
+      <Container>
+        <Navbar.Brand>
+          <img
+            src={logo}
+            alt=""
+            style={{ maxWidth: "100px", height: "100%" }}
+          />
+        </Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+          <Navbar.Text style={{ color: "black", fontWeight: "600" }}>
+            Go Back :{" "}
+            <a
+              style={{
+                textDecoration: "none",
+                color: "black",
+                fontWeight: "600",
+              }}
+              href="/"
+            >
+              Home
+            </a>
+          </Navbar.Text>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
 
-export default NavMenu
+export default NavMenu;
