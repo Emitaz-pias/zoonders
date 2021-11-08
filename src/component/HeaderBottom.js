85bab0094b96b440c36e6610d46a76e0c689323e
Emitaz-pias <61836021+Emitaz-pias@users.noreply.github.com>
2021-11-08 21:29:19 +0600

    new responsive issue fixed

--- src/component/HeaderBottom.js
+++ src/component/HeaderBottom.js
@@ -3,18 +3,6 @@ import { Container, Row, Col } from "react-bootstrap";
 
 const HeaderBottom = () => {
   return (
-<<<<<<< HEAD
-    <div className="headerBottom">
-      <Container>
-        <Row className="mt-1 headerBottom">
-          <Col md={12}>
-            <h2 className="discover">The Future Starts Today !</h2>
-          </Col>
-        </Row>
-      </Container>
-    </div>
-    
-=======
     <Container className="headerBottom" style={{ marginTop: "-0.2rem" }}>
       <Row className="mt-1 headerBottom">
         <Col md={12}>
@@ -22,7 +10,6 @@ const HeaderBottom = () => {
         </Col>
       </Row>
     </Container>
->>>>>>> a5713c6bb668793e00ed7a3e75b7a15aa61d8de0
   );
 };
 
